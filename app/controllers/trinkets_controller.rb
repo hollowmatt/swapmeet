14d383ea7249c6300a8fadad99016a16872e271c
Matt Holloway <hollowmatt@gmail.com>
2018-03-07 21:15:32 -0500

    playing around a bit

--- app/controllers/trinkets_controller.rb
+++ app/controllers/trinkets_controller.rb
@@ -1,2 +1,23 @@
 class TrinketsController < ApplicationController
+	def index
+		@trinkets = Trinket.all
+	end
+
+	def new
+		@trinket = new Trinket
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	private
+		def trinket_params
+			params.require(:trinket).permit(:title, :description, :photo_path, :price)
+		end
 end
--- db/migrate/20180305233303_create_trinkets.rb
+++ db/migrate/20180305233303_create_trinkets.rb
@@ -1,7 +1,10 @@
 class CreateTrinkets < ActiveRecord::Migration[5.0]
   def change
     create_table :trinkets do |t|
-
+    	t.string :title
+    	t.text :description
+    	t.string :photo_path
+    	t.decimal :price
       t.timestamps
     end
   end
