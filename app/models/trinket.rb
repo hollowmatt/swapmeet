547d3ada9ca07028b67bb0ae663c619427d2e312
Matt Holloway <hollowmatt@gmail.com>
2018-03-13 07:17:52 -0400

    added some model level validation

--- app/models/trinket.rb
+++ app/models/trinket.rb
@@ -1,3 +1,8 @@
 class Trinket < ApplicationRecord
 	mount_uploader :photo_path, ImageUploader
+	validates :photo_path, presence: true
+	validates :description, presence: true
+	validates :description, length: { minimum: 5 }
+	validates :price, presence: true
+	validates :price, numericality: true
 end
