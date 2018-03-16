ab90d69ee0a98586bd1fa110ef057f3447170f69
Matt Holloway <hollowmatt@gmail.com>
2018-03-16 12:36:09 -0400

    Added more Model tests

--- spec/models/trinket_spec.rb
+++ spec/models/trinket_spec.rb
@@ -5,8 +5,17 @@ describe Trinket do
 		trinket = create(:trinket)
 		expect(trinket).to be_valid
 	end
-	it "is invalid without a description"
-	it "is invalid without a photo_path"
-	it "is invalid without a price"
+	
+	it "is invalid without a description" do
+		FactoryBot.build(:trinket, description: nil).should_not be_valid
+	end
+
+	it "is invalid without a photo_path" do
+		FactoryBot.build(:trinket, photo_path: nil).should_not be_valid
+	end
+
+	it "is invalid without a price" do
+		FactoryBot.build(:trinket, price: nil).should_not be_valid
+	end
 end
 	
