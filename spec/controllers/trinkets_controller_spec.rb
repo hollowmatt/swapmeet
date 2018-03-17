7e53fea42acfff89c7d09332610b544116ec79f1
Matt Holloway <hollowmatt@gmail.com>
2018-03-17 15:40:10 -0400

    Will eventually replace controller tests with feature specs... but for now, added gem and first controller test

--- spec/controllers/trinkets_controller_spec.rb
+++ spec/controllers/trinkets_controller_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 describe TrinketsController do
 
 	describe 'GET #index' do 
-		it "renders the index view"
+		it "renders the index view" do 
+			get :index
+			expect(response).to render_template(:index)
+		end
 	end
 	describe 'GET #show' do
 		it "renders the show view"
