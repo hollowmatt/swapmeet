c0f65d184e1c5f9a35cb690ad88f80b85a1a10e7
Matt Holloway <hollowmatt@gmail.com>
2018-03-17 08:24:45 -0400

    Start framing up controller tests for trinkets

---
+++ spec/controllers/trinkets_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe TrinketsController do
+
+	describe 'GET #index' do 
+		it "renders the index view"
+	end
+	describe 'GET #show' do
+		it "renders the show view"
+	end
+
+	describe 'PATCH #update'
+	describe 'POST #create'
+	describe 'DESTROY #delete'
+
+end
