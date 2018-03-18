aa4e7d30d06683f6a39d404546e09475f28636ab
Matt Holloway <hollowmatt@gmail.com>
2018-03-18 16:48:06 -0400

    added create and update tests

--- spec/controllers/trinkets_controller_spec.rb
+++ spec/controllers/trinkets_controller_spec.rb
@@ -8,6 +8,7 @@ describe TrinketsController do
 			expect(response).to render_template(:index)
 		end
 	end
+	
 	describe 'GET #show' do
 		it "renders the show view" do
 			@trinket = create(:trinket)
@@ -16,8 +17,34 @@ describe TrinketsController do
 		end
 	end
 
-	describe 'PATCH #update'
-	describe 'POST #create'
-	describe 'DESTROY #delete'
+	describe 'POST #create' do 
+		before do
+			@trinkets = Trinket.all
+		end
+		it "creates a trinket" do 
+			trinket_params = FactoryBot.attributes_for(:trinket)
+			expect {
+        post :create, params: { trinket: trinket_params }
+       }.to change(@trinkets, :count).by(1)
+		end
+	end	
+	
+	describe 'PATCH #update' do
+		before do
+			@trinket = create(:trinket)
+		end
+		it "updates a trinket" do
+			trinket_params = FactoryBot.attributes_for(:trinket, description: "New Description")
+			patch :update, params: {id: @trinket.id, trinket: trinket_params }
+			expect(@trinket.reload.description).to eq "New Description"
+		end
+	end
+	
+	describe 'DESTROY #delete' do
+		before do
+			@trinket = create(:trinket)
+		end
+	end
+
 
 end
